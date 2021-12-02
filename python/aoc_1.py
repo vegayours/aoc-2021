532b34e46a68f9bcdd0bd6f6a3eb6cafe7db4b6b
Aleksandr Likhanov <vegayours@gmail.com>
2021-12-03 00:01:19 +0100

    Simplify python Day 1 solution.

--- python/aoc_1.py
+++ python/aoc_1.py
@@ -1,12 +1,4 @@
-from itertools import tee
-
-
-def iterate_tuples(iterable, n):
-    sequences = tee(iterable, n)
-    for i in range(len(sequences)):
-        for _ in range(i):
-            next(sequences[i])
-    return zip(*sequences)
+from more_itertools import windowed
 
 
 def count_greater_than_prev(pairs):
@@ -15,16 +7,14 @@ def count_greater_than_prev(pairs):
 
 def solve_1(lines):
     nums = map(int, lines)
-    pairs = iterate_tuples(nums, 2)
-    return count_greater_than_prev(pairs)
+    pairs = windowed(nums, 2)
+    return sum(map(lambda t: t[1] > t[0], pairs))  # type: ignore
 
 
 def solve_2(lines):
     nums = map(int, lines)
-    triplets = iterate_tuples(nums, 3)
-    window_nums = map(sum, triplets)
-    window_pairs = iterate_tuples(window_nums, 2)
-    return count_greater_than_prev(window_pairs)
+    pairs = windowed(nums, 4)
+    return sum(map(lambda t: t[3] > t[0], pairs))  # type: ignore
 
 
 def solution():
