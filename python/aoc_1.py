854c74dd6cfdc811ad32557852232a3e8d2c8e19
Aleksandr Likhanov <vegayours@gmail.com>
2021-12-02 23:18:33 +0100

    Add python solution for Day 1.

--- python/aoc_1.py
+++ python/aoc_1.py
@@ -1,9 +1,30 @@
+from itertools import tee
+
+
+def iterate_tuples(iterable, n):
+    sequences = tee(iterable, n)
+    for i in range(len(sequences)):
+        for _ in range(i):
+            next(sequences[i])
+    return zip(*sequences)
+
+
+def count_greater_than_prev(pairs):
+    return sum(map(lambda t: t[1] > t[0], pairs))
+
+
 def solve_1(lines):
-    pass
+    nums = map(int, lines)
+    pairs = iterate_tuples(nums, 2)
+    return count_greater_than_prev(pairs)
 
 
 def solve_2(lines):
-    pass
+    nums = map(int, lines)
+    triplets = iterate_tuples(nums, 3)
+    window_nums = map(sum, triplets)
+    window_pairs = iterate_tuples(window_nums, 2)
+    return count_greater_than_prev(window_pairs)
 
 
 def solution():
